2b84d91012cd2a129d0447edb7e9ac7c14ed8be4
Mohammad Aneek <aneek788@gmail.com>
2025-12-19 00:37:49 +0530

    [Mohammad Aneek] Add program to count digits in a number

---
+++ gcr-codebase/control-flow/level-3/CountDigits.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        // Create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // get a number as input
+        int number = input.nextInt();
+
+        //check for positive
+        if (number < 0) {
+            System.out.println("Invalid number");
+            input.close();
+            return;
+        }
+
+        // check for number of digits
+        int counter = 0;
+        int temporaryNumber = number;
+
+        // loop continues while there is atleast 1 digit
+        while (temporaryNumber > 0) {
+            // increase counter to depict 1 digit
+            counter++;
+
+            // removing last digit from the number
+            temporaryNumber/=10;
+        }
+
+        // display the results
+        System.out.println("There are "+counter+" digits in "+number);
+        // close the Scanner
+        input.close();
+    }
+}
