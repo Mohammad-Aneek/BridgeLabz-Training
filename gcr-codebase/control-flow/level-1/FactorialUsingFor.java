6a621f4feee0315306239041ea9b882aef8b06a5
Mohammad Aneek <aneek788@gmail.com>
2025-12-18 14:40:38 +0530

    [Mohammad Aneek] Add program to find factorial of a number using for loop

---
+++ gcr-codebase/control-flow/level-1/FactorialUsingFor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FactorialUsingFor {
+    public static void main(String[] args) {
+        // creating a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // number to find factorial of
+        int number = input.nextInt();
+
+        // check the number
+        if (number < 0) {
+            System.out.println("Invalid number");
+            return;
+        }
+
+        // calculate the factorial
+        int factorial = 1;
+
+        for (int counter = 2; counter<=number; counter++) {
+            factorial*=counter;
+        }
+
+        // display the result
+        System.out.println("The factorial of " + number + " is " + factorial);
+
+        // closing the Scanner
+        input.close();
+    }
+}
