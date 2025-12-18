2c30c90a04f61c9db92a4d96a6910976ba0d49b1
Mohammad Aneek <aneek788@gmail.com>
2025-12-18 13:01:18 +0530

    [Mohammad Aneek] Add program to check a number

---
+++ gcr-codebase/control-flow/level-1/CheckNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        // creating a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // number to be checked
+        int number = input.nextInt();
+
+        // check the number
+        if (number < 0) {
+            System.out.println("negative");
+        } else if(number > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("zero");
+        }
+
+        // closing the Scanner
+        input.close();
+    }
+}
