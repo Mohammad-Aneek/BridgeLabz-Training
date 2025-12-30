b1e3e6824f0189056a69a084750edfba56f4a93c
Mohammad Aneek <aneek788@gmail.com>
2025-12-30 15:03:45 +0530

    [Mohammad Aneek] Update file to include the problem statement

--- scenario-based-codebase/day-03/ParkingLotGateSystem.java
+++ scenario-based-codebase/day-03/ParkingLotGateSystem.java
@@ -1,3 +1,10 @@
+/* 6. Parking Lot Gate System 🚗
+ * Develop a smart parking system.
+ * ● Options: Park, Exit, Show Occupancy
+ * ● Use switch-case for the menu.
+ * ● while loop to continue until the parking lot is full or the user exits.
+*/
+
 import java.util.Scanner;
 
 public class ParkingLotGateSystem {
