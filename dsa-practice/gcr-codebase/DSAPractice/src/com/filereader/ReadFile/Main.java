90f9e59fb12a73020274db11e11901b4d2b697b1
Mohammad Aneek <aneek788@gmail.com>
2026-01-12 02:59:51 +0530

    [Mohammad Aneek] Add program files using FileReader to read a file line by line

---
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/filereader/ReadFile/Main.java
@@ -0,0 +1,19 @@
+package com.filereader.ReadFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new FileReader("src/com/filereader/ReadFile/Text.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + e.getMessage());
+		}
+	}
+}
