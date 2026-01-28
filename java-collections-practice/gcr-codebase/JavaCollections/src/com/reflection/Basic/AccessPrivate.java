eb7150f92736c09e61a01828f6fe16d6b843038b
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 15:49:47 +0530

    [Mohammad Aneek] Add the Java Reflection program file for AccessPrivate problem

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflection/Basic/AccessPrivate.java
@@ -0,0 +1,18 @@
+package com.reflection.Basic;
+
+import java.lang.reflect.Field;
+
+public class AccessPrivate {
+
+	public static void main(String[] args) throws Exception {
+		Person p = new Person();
+
+		Class<?> clazz = p.getClass();
+
+		Field ageField = clazz.getDeclaredField("age");
+		ageField.setAccessible(true);
+		System.out.println("Age before modification: " + ageField.getInt(p));
+		ageField.setInt(p, 30);
+		System.out.println("Age after modification: " + ageField.getInt(p));
+	}
+}
