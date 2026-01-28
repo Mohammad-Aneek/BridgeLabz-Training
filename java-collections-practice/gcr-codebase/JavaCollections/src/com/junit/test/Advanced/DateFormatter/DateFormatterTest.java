03b41512974b83b4c6067eaf2e2ec6ab1462fd60
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 15:18:33 +0530

    [Mohammad Aneek] Add the JUnit main program file and test file for DateFormatter problem test

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/junit/test/Advanced/DateFormatter/DateFormatterTest.java
@@ -0,0 +1,23 @@
+package com.junit.test.Advanced.DateFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.junit.main.Advanced.DateFormatter.DateFormatter;
+
+class DateFormatterTest {
+	DateFormatter formatter = new DateFormatter();
+
+	@Test
+	void testFormatDate() {
+		assertEquals("25-12-2023", formatter.formatDate("2023-12-25"));
+	}
+
+	@Test
+	void testInvalidDateFormat() {
+		assertThrows(Exception.class, () -> {
+			formatter.formatDate("12-25-2023");
+		});
+	}
+}
