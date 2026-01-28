f26a39415cafce2626600a0a94f2ad8e66cabfe1
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 15:51:16 +0530

    [Mohammad Aneek] Add the Java Reflection program file for InvokePrivate problem

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflection/Basic/InvokePrivate.java
@@ -0,0 +1,16 @@
+package com.reflection.Basic;
+
+import java.lang.reflect.Method;
+
+public class InvokePrivate {
+	public static void main(String[] args) throws Exception {
+		Person p = new Person();
+
+		Class<?> clazz = p.getClass();
+
+		Method displayMethod = clazz.getDeclaredMethod("displayInfo");
+		displayMethod.setAccessible(true);
+
+		displayMethod.invoke(p);
+	}
+}
