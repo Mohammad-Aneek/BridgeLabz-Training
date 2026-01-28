f16bda9c392cc850f3bd1c18a9107b0362e8407c
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 14:42:58 +0530

    [Mohammad Aneek] Add the Java Exception Handling program files for UncheckedException problem

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/UncheckedException/Main.java
@@ -0,0 +1,20 @@
+package com.exceptions.UncheckedException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.print("Enter two numbers: ");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println("Result: " + (a / b));
+		} catch (ArithmeticException e) {
+			System.out.println("Cannot divide by zero!");
+		} catch (InputMismatchException e) {
+			System.out.println("Please enter numeric values only.");
+		}
+	}
+}
