2ca4950fbc1df4bb20263ffd2ff1fa187d41ba47
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 16:21:39 +0530

    [Mohammad Aneek] Add the Java Annotation program files for Marker problem

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/annotations/Beginner/Marker/Main.java
@@ -0,0 +1,15 @@
+package com.annotations.Beginner.Marker;
+
+import java.lang.reflect.Method;
+
+public class Main {
+	public static void main(String[] args) {
+		Method[] methods = ReportSystem.class.getDeclaredMethods();
+
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(ImportantMethod.class)) {
+				System.out.println("Important Method: " + method.getName());
+			}
+		}
+	}
+}
