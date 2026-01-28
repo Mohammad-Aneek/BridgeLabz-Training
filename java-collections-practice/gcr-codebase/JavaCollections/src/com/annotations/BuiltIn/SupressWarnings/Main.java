219c4beccf19865ac45808a866768a55f9fe00a2
Mohammad Aneek <aneek788@gmail.com>
2026-01-28 16:23:58 +0530

    [Mohammad Aneek] Add the Java Annotation program files for SupressWarnings problem

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/annotations/BuiltIn/SupressWarnings/Main.java
@@ -0,0 +1,13 @@
+package com.annotations.BuiltIn.SupressWarnings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		List myList = new ArrayList();
+		myList.add("Hello");
+		System.out.println(myList.get(0));
+	}
+}
