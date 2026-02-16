cedb9f560920a718bcf88d3df3766bec88cf2ced
Mohammad Aneek <aneek788@gmail.com>
2026-02-16 13:13:50 +0530

    [Mohammad Aneek] Add the Java Design Principle program file named Librarian for Library Management System Problem

---
+++ java-design-pattern-practice/gcr-codebase/DesignPractice/src/com/LibrarySystem/Librarian.java
@@ -0,0 +1,19 @@
+package com.LibrarySystem;
+
+public class Librarian implements User {
+	private String name;
+
+	public Librarian(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void showRole() {
+		System.out.println(name + " is a Librarian.");
+	}
+
+	@Override
+	public void update(String msg) {
+		System.out.println(name + " notified: " + msg);
+	}
+}
